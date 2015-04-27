a76ef7c7eddac222d23d385b03362934b937d557
GarrusN7 <rsbanjoman@gmail.com>
2015-04-27 14:46:38 -0600

    initial commit

---
+++ app/controllers/staticpages_controller.rb
@@ -0,0 +1,16 @@
+class StaticpagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def movies
+  end
+
+  def artwork
+  end
+end
---
+++ test/controllers/staticpages_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class StaticpagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+  end
+
+  test "should get movies" do
+    get :movies
+    assert_response :success
+  end
+
+  test "should get artwork" do
+    get :artwork
+    assert_response :success
+  end
+
+end
